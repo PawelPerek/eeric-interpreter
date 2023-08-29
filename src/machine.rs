4a2f6ffbcabb33d88e625aa101aa41a007650771
PawelPerek <pawelperek99@gmail.com>
2023-08-29 02:10:17 +0200

    Restrict r, i, s, u, r4 format parsers

--- src/machine.rs
+++ src/machine.rs
@@ -1,19 +1,35 @@
 mod decoder;
 
+use std::collections::HashMap;
+
 use decoder::Decoder;
 
 pub struct RvMachine {
-    core: eeric::RvCore,
-    decoder: Decoder
+    core: eeric::RvCore
 }
 
 impl RvMachine {
     pub fn new() -> RvMachine {
         RvMachine {
-            core: eeric::RvCore::new_zeroed(),
-            decoder: Decoder
+            core: eeric::RvCore::new_zeroed()
         }
     }
 
+    pub fn compile(&mut self, program: String) -> Vec<eeric::Instruction> {
+        let mut labels: HashMap<&str, u64> = HashMap::new();
+        let mut line_address = 0;
 
+        for line in program.lines() {
+            let mut tokens = line.split_whitespace();
+            let first_token = tokens.next().unwrap();
+            if first_token.ends_with(":") {
+                let label = first_token.trim_end_matches(":");
+                labels.insert(label, line_address);
+            }
+            line_address += 4;
+        }
+
+        let mut class = Decoder::classify(instruction);
+        decoder.decode()
+    }
 }
--- src/machine/decoder/operand.rs
+++ src/machine/decoder/operand.rs
@@ -1,44 +1,243 @@
+pub struct IntegerParser;
 
-pub fn parse_operand(str_repr: &str) -> Result<usize, &'static str> {
-    let mut tidy_str = str_repr.trim().to_lowercase().trim_end_matches(",").to_string();
+impl IntegerParser {
+    pub fn parse_r_format(r: &str) -> Result<eeric::Format::R, String> {
+        let tokens: Vec<&str> = r.split(", ").collect();
 
-    if tidy_str.starts_with("(") && tidy_str.ends_with(")") {
-        tidy_str = tidy_str[1..tidy_str.len() - 1].to_string();
+        if tokens.len() != 3 {
+            return Err("Expected format: 'rd, rs1, rs2'".to_owned());
+        }
+
+        let rd = tokens[0];
+        let rs1 = tokens[1];
+        let rs2 = tokens[2];
+
+        let rd = Self::parse_operand(rd)?;
+        let rs1 = Self::parse_operand(rs1)?;
+        let rs2 = Self::parse_operand(rs2)?;
+
+        Ok(eeric::Format::R { rd, rs1, rs2 })
     }
 
-    match tidy_str.to_lowercase().as_str() {
-        "x0" | "zero" | "f0" | "ft0" | "v0" | "v0.t" => Ok(0),
-        "x1" | "ra" | "f1" | "ft1" | "v1" => Ok(1),
-        "x2" | "sp" | "f2" | "ft2" | "v2" => Ok(2),
-        "x3" | "gp" | "f3" | "ft3" | "v3" => Ok(3),
-        "x4" | "tp" | "f4" | "ft4" | "v4" => Ok(4),
-        "x5" | "t0" | "f5" | "ft5" | "v5" => Ok(5),
-        "x6" | "t1" | "f6" | "ft6" | "v6" => Ok(6),
-        "x7" | "t2" | "f7" | "ft7" | "v7" => Ok(7),
-        "x8" | "s0" | "fp" | "f8" | "fs0" | "v8" => Ok(8),
-        "x9" | "s1" | "f9" | "fs1" | "v9" => Ok(9),
-        "x10" | "a0" | "f10" | "fa0" | "v10" => Ok(10),
-        "x11" | "a1" | "f11" | "fa1" | "v11" => Ok(11),
-        "x12" | "a2" | "f12" | "fa2" | "v12" => Ok(12),
-        "x13" | "a3" | "f13" | "fa3" | "v13" => Ok(13),
-        "x14" | "a4" | "f14" | "fa4" | "v14" => Ok(14),
-        "x15" | "a5" | "f15" | "fa5" | "v15" => Ok(15),
-        "x16" | "a6" | "f16" | "fa6" | "v16" => Ok(16),
-        "x17" | "a7" | "f17" | "fa7" | "v17" => Ok(17),
-        "x18" | "s2" | "f18" | "fs2" | "v18" => Ok(18),
-        "x19" | "s3" | "f19" | "fs3" | "v19" => Ok(19),
-        "x20" | "s4" | "f20" | "fs4" | "v20" => Ok(20),
-        "x21" | "s5" | "f21" | "fs5" | "v21" => Ok(21),
-        "x22" | "s6" | "f22" | "fs6" | "v22" => Ok(22),
-        "x23" | "s7" | "f23" | "fs7" | "v23" => Ok(23),
-        "x24" | "s8" | "f24" | "fs8" | "v24" => Ok(24),
-        "x25" | "s9" | "f25" | "fs9" | "v25" => Ok(25),
-        "x26" | "s10" | "f26" | "fs10" | "v26" => Ok(26),
-        "x27" | "s11" | "f27" | "fs11" | "v27" => Ok(27),
-        "x28" | "t3" | "f28" | "ft8" | "v28" => Ok(28),
-        "x29" | "t4" | "f29" | "ft9" | "v29" => Ok(29),
-        "x30" | "t5" | "f30" | "ft10" | "v30" => Ok(30),
-        "x31" | "t6" | "f31" | "ft11" | "v31" => Ok(31),
-        maybe_immediate => maybe_immediate.parse::<usize>().map_err(|_| "Invalid operand")
+    pub fn parse_i_format(i: &str) -> Result<eeric::Format::I, String> {
+        let tokens: Vec<&str> = i.split(", ").collect();
+
+        if tokens.len() != 3 {
+            return Err("Expected format: 'rd, rs1, imm'".to_owned());
+        }
+
+        let rd = tokens[0];
+        let rs1 = tokens[1];
+        let imm = tokens[2];
+
+        let rd = Self::parse_operand(rd)?;
+        let rs1 = Self::parse_operand(rs1)?;
+        let imm = Self::parse_immediate(imm)?;
+
+        Ok(eeric::Format::I { rd, rs1, imm12: imm })
+    }
+
+    pub fn parse_s_format(s: &str) -> Result<eeric::Format::S, String> {
+        let tokens: Vec<&str> = s.split(", ").collect();
+
+        if tokens.len() != 2 {
+            return Err("Expected format: 'rs2, immediate(rs1)'".to_string());
+        }
+
+        let rs2 = tokens[0].trim();
+        let imm_rs1 = tokens[1].trim();
+
+        if !imm_rs1.ends_with(")") {
+            return Err("Malformed format: missing closing ')'".to_string());
+        }
+
+        let inner_part = &imm_rs1[..imm_rs1.len() - 1];  // Remove the closing ')'
+        let parts: Vec<&str> = inner_part.split('(').collect();
+
+        if parts.len() != 2 {
+            return Err("Expected format: 'immediate(rs1)' for the second part".to_string());
+        }
+
+        let imm = parts[0].trim();
+        let rs1 = parts[1].trim();
+
+        let rs1 = Self::parse_operand(rs1)?;
+        let rs2 = Self::parse_operand(rs2)?;
+        let imm = Self::parse_immediate(imm)?;
+
+        Ok(eeric::Format::S { rs1, rs2, imm12: imm })
+    }
+
+    pub fn parse_u_format(u: &str) -> Result<eeric::Format::U, String> {
+        let tokens: Vec<&str> = u.split(", ").collect();
+
+        if tokens.len() != 2 {
+            return Err("Expected format: 'rd, imm'".to_owned());
+        }
+
+        let rd = tokens[0];
+        let imm = tokens[1];
+
+        let rd = Self::parse_operand(rd)?;
+        let imm = Self::parse_immediate(imm)?;
+
+        Ok(eeric::Format::U { rd, imm20: imm })
+    }
+
+    pub(self) fn parse_operand(op_str: &str) -> Result<usize, String> {
+        match op_str {
+            "x0"  | "zero"        => Ok(0),
+            "x1"  | "ra"          => Ok(1),
+            "x2"  | "sp"          => Ok(2),
+            "x3"  | "gp"          => Ok(3),
+            "x4"  | "tp"          => Ok(4),
+            "x5"  | "t0"          => Ok(5),
+            "x6"  | "t1"          => Ok(6),
+            "x7"  | "t2"          => Ok(7),
+            "x8"  | "s0"   | "fp" => Ok(8),
+            "x9"  | "s1"          => Ok(9),
+            "x10" | "a0"          => Ok(10),
+            "x11" | "a1"          => Ok(11),
+            "x12" | "a2"          => Ok(12),
+            "x13" | "a3"          => Ok(13),
+            "x14" | "a4"          => Ok(14),
+            "x15" | "a5"          => Ok(15),
+            "x16" | "a6"          => Ok(16),
+            "x17" | "a7"          => Ok(17),
+            "x18" | "s2"          => Ok(18),
+            "x19" | "s3"          => Ok(19),
+            "x20" | "s4"          => Ok(20),
+            "x21" | "s5"          => Ok(21),
+            "x22" | "s6"          => Ok(22),
+            "x23" | "s7"          => Ok(23),
+            "x24" | "s8"          => Ok(24),
+            "x25" | "s9"          => Ok(25),
+            "x26" | "s10"         => Ok(26),
+            "x27" | "s11"         => Ok(27),
+            "x28" | "t3"          => Ok(28),
+            "x29" | "t4"          => Ok(29),
+            "x30" | "t5"          => Ok(30),
+            "x31" | "t6"          => Ok(31),
+            _                     => Err(format!("Incorrect integer operand: {}", op_str))   
+        }
+    }
+
+    pub(self) fn parse_immediate(imm_str: &str) -> Result<u64, String> {
+        if imm_str.starts_with("0x") || imm_str.starts_with("0X") {
+            u64::from_str_radix(&imm_str[2..], 16).map_err(|e| format!("Error parsing immediate: {}", e))
+        } else {
+            imm_str.parse::<u64>().map_err(|e| format!("Error parsing immediate: {}", e))
+        }
+    }
+}
+
+pub struct FloatParser;
+
+impl FloatParser {
+    pub fn parse_r4_format(r: &str) -> Result<eeric::Format::R4, String> {
+        let tokens: Vec<&str> = r.split(", ").collect();
+
+        if tokens.len() != 4 {
+            return Err("Expected format: 'rd, rs1, rs2, rs3'".to_owned());
+        }
+
+        let rd = tokens[0];
+        let rs1 = tokens[1];
+        let rs2 = tokens[2];
+        let rs3 = tokens[3];
+
+        let rd = Self::parse_operand(rd)?;
+        let rs1 = Self::parse_operand(rs1)?;
+        let rs2 = Self::parse_operand(rs2)?;
+        let rs3 = Self::parse_operand(rs3)?;
+
+        Ok(eeric::Format::R4 { rd, rs1, rs2, rs3 })
+    }
+
+    pub(self) fn parse_operand(op_str: &str) -> Result<usize, String> {
+        match op_str {
+            "f0"  | "ft0"  => Ok(0),
+            "f1"  | "ft1"  => Ok(1),
+            "f2"  | "ft2"  => Ok(2),
+            "f3"  | "ft3"  => Ok(3),
+            "f4"  | "ft4"  => Ok(4),
+            "f5"  | "ft5"  => Ok(5),
+            "f6"  | "ft6"  => Ok(6),
+            "f7"  | "ft7"  => Ok(7),
+            "f8"  | "fs0"  => Ok(8),
+            "f9"  | "fs1"  => Ok(9),
+            "f10" | "fa0"  => Ok(10),
+            "f11" | "fa1"  => Ok(11),
+            "f12" | "fa2"  => Ok(12),
+            "f13" | "fa3"  => Ok(13),
+            "f14" | "fa4"  => Ok(14),
+            "f15" | "fa5"  => Ok(15),
+            "f16" | "fa6"  => Ok(16),
+            "f17" | "fa7"  => Ok(17),
+            "f18" | "fs2"  => Ok(18),
+            "f19" | "fs3"  => Ok(19),
+            "f20" | "fs4"  => Ok(20),
+            "f21" | "fs5"  => Ok(21),
+            "f22" | "fs6"  => Ok(22),
+            "f23" | "fs7"  => Ok(23),
+            "f24" | "fs8"  => Ok(24),
+            "f25" | "fs9"  => Ok(25),
+            "f26" | "fs10" => Ok(26),
+            "f27" | "fs11" => Ok(27),
+            "f28" | "ft8"  => Ok(28),
+            "f29" | "ft9"  => Ok(29),
+            "f30" | "ft10" => Ok(30),
+            "f31" | "ft11" => Ok(31),
+            _              => Err(format!("Incorrect float operand: {}", op_str))  
+        }
+    }
+}
+
+
+
+
+enum VectorOperand {
+    Register(usize),
+    Mask
+}
+
+pub fn parse_vector_operand(op: String) -> Result<VectorOperand, String> {
+    use VectorOperand::*;
+    
+    match op.as_str() {
+        "v0.t" => Ok(Mask),
+        "v0"   => Ok(Register(0)),
+        "v1"   => Ok(Register(1)),
+        "v2"   => Ok(Register(2)),
+        "v3"   => Ok(Register(3)),
+        "v4"   => Ok(Register(4)),
+        "v5"   => Ok(Register(5)),
+        "v6"   => Ok(Register(6)),
+        "v7"   => Ok(Register(7)),
+        "v8"   => Ok(Register(8)),
+        "v9"   => Ok(Register(9)),
+        "v10"  => Ok(Register(10)),
+        "v11"  => Ok(Register(11)),
+        "v12"  => Ok(Register(12)),
+        "v13"  => Ok(Register(13)),
+        "v14"  => Ok(Register(14)),
+        "v15"  => Ok(Register(15)),
+        "v16"  => Ok(Register(16)),
+        "v17"  => Ok(Register(17)),
+        "v18"  => Ok(Register(18)),
+        "v19"  => Ok(Register(19)),
+        "v20"  => Ok(Register(20)),
+        "v21"  => Ok(Register(21)),
+        "v22"  => Ok(Register(22)),
+        "v23"  => Ok(Register(23)),
+        "v24"  => Ok(Register(24)),
+        "v25"  => Ok(Register(25)),
+        "v26"  => Ok(Register(26)),
+        "v27"  => Ok(Register(27)),
+        "v28"  => Ok(Register(28)),
+        "v29"  => Ok(Register(29)),
+        "v30"  => Ok(Register(30)),
+        "v31"  => Ok(Register(31)),
+        _              => Err(op)   
     }
 }
