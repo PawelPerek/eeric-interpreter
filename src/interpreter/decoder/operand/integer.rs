9ed31b48e1df4e2f340f4a64025767e2e626c37d
PawelPerek <pawelperek99@gmail.com>
2023-09-09 00:13:38 +0200

    Bump eeric

--- src/interpreter/decoder/operand/integer.rs
+++ src/interpreter/decoder/operand/integer.rs
@@ -238,7 +238,7 @@ pub fn parse_label(
 ) -> Result<i32, String> {
     map.get(label)
         .cloned()
-        .map(|addr| addr.wrapping_sub(current_line).wrapping_sub(4) as i32)
+        .map(|addr| addr.wrapping_sub(current_line) as i32)
         .ok_or(format!("Did not find label {}", label))
 }
 
